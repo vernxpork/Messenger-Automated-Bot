85766f4d71cd6af0ca90b246c2a8a797f34fb7fb
vernesg <veaxdev36@gmail.com>
2025-07-09 22:43:25 +0800

    Create guide.js

---
+++ public/guide.js
@@ -0,0 +1,40 @@
+const guidez =`
+         <h1 class="col text-center" style="font-size: 20px; padding: 10px;">Step-By-Step Guide</h1>
+         <p>1. Install the Kiwi Browser from the Google Play Store and proceed to launch the application.</p>
+         <img src="image/guide_1.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+         <p>2. Upon launching the app, navigate to the search function and enter the following link: <a href="https://github.com/c3cbot/c3c-fbstate/archive/refs/tags/1.5.zip">https://github.com/c3cbot/c3c-fbstate/archive/refs/tags/1.5.zip</a></p>
+         <img src="image/guide_2.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+         <p>3. Once entered, a prompt will appear instructing you to download the file; proceed by clicking the download option.</p>
+         <img src="image/guide_3.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+         <p>4. After completing the download, tap the three dots in the upper-right corner, then select the "Extensions" option.</p>
+         <img src="image/guide_4.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+         <p>5. In the Extensions menu, enable Developer Mode, then click the "+" button to add the extension from either a zip file, crx file, or user.js file.</p>
+         <img src="image/guide_5.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+         <p>6. Locate the previously downloaded zip file and click on it to initiate the installation process.</p>
+         <img src="image/guide_6.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+         <p>7. Once the installation is complete, observe the C3C fbstate utility in the bottom-right corner and activate it by clicking on it.</p>
+         <img src="image/guide_7.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+         <p>8. Upon activation, navigate to Facebook.com using the Kiwi Browser and log in your account.</p>
+         <img src="image/guide_8.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+         <p>9. After logging in, access the menu by clicking the three dots in the upper right corner, scroll down, and locate the C3C fbstate utility.</p>
+         <img src="image/guide_9.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+         <p>10. You'll be redirected to another page; locate the "Copy Clipboard" option and click it to copy your cookies for further use.</p>
+         <img src="image/guide_10.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+         <p>11. Paste your copied cookies at <a href="https://fbbot69-6g37.onrender.com">fbbot69-6g37.onrender.com</a>, then scroll down to thoroughly review the Terms and Conditions and Privacy Policy.</p>
+         <img src="image/guide_11.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+         <p>12. Finally, having carefully reviewed the Terms and Conditions and Privacy Policy, check the box to confirm your understanding and submit the information.</p>
+         <img src="image/guide_12.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+         <p>13. Certainly, please input "ai" to confirm the bot's operational status. If it responds, congratulations on successful verification.</p>
+         <img src="image/guide_13.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+         <br>
+         <h1 class="col text-center" style="font-size: 20px;">Note:</h1>
+         <p>• In case the bot encounters a login issue during the initial log, it may be due to a lock. Simply unlock it and repeat the process.</p>
+         <p>• If the bot is already logged into Facebook/FB Lite, refrain from logging in on other devices or initiating a logout.</p>
+         <p>• While the bot is active, avoid logging into the bot account or changing the password.</p>
+         <p>• Please be aware that there is a possibility of encountering account locks or suspensions, and we are not responsible for such occurrences.</p>
+         <p>• Please be aware that there is a possibility of encountering account locks or suspensions, and we are not responsible for such occurrences.</p>
+         <p> To register another bot/Al, simply tap on "logout" (not die appstate) to acquire a new appstate for another account.</p>
+         <img src="image/guide_16.jpeg" alt="" width="250" height="200" style="display: block; margin: auto; border-radius: 10px; margin: 10px auto;">
+      </div>
+     `;
+      document.getElementById("guidez").innerHTML = guidez;
