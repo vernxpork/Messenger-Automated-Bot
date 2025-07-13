1559aa6b77e51499da5c6a3c2217edf48ad80868
vernesg <veaxdev36@gmail.com>
2025-07-13 08:34:27 +0800

    Update uid.js

--- script/uid.js
+++ script/uid.js
@@ -1,26 +1,54 @@
 module.exports.config = {
-	name: "uid",
-	role: 0,
-	credits: "Vern",
-	description: "Get the user's Facebook UID.",
-	hasPrefix: false,
-	usages: "{p}uid {p}uid @mention",
-	cooldown: 5,
-	aliases: ["id","ui"]
+  name: 'uid',
+  version: '1.0.0',
+  role: 0,
+  hasPrefix: false,
+  aliases: ['userid', 'getid'],
+  description: 'Get user or group ID',
+  usage: 'uid [id/reply/group/all]',
+  credits: 'Vern',
+  cooldown: 3,
 };
 
-module.exports.run = async function({ api, event }) {
-	if (Object.keys(event.mentions).length === 0) {
-		if (event.messageReply) {
-			const senderID = event.messageReply.senderID;
-			return api.sendMessage(senderID, event.threadID);
-		} else {
-			return api.sendMessage(`${event.senderID}`, event.threadID, event.messageID);
-		}
-	} else {
-		for (const mentionID in event.mentions) {
-			const mentionName = event.mentions[mentionID];
-			api.sendMessage(`${mentionName.replace('@', '')}: ${mentionID}`, event.threadID);
-		}
-	}
+module.exports.run = async function({ api, event, args }) {
+  const { threadID, senderID, type, messageReply, participantIDs, mentions } = event;
+  let id;
+
+  if (!args[0]) {
+    id = senderID;
+  }
+
+  if (args[0]) {
+    if (args[0].startsWith('https://')) {
+      try {
+        const uid = await api.getUID(args[0]);
+        return api.shareContact(uid, uid, threadID);
+      } catch (error) {
+        return api.sendMessage('Invalid URL or unable to retrieve UID.', threadID);
+      }
+    }
+  }
+
+  if (type === 'message_reply') {
+    id = messageReply.senderID;
+  }
+
+  const input = args.join(' ');
+  if (input.includes('@')) {
+    id = Object.keys(mentions)[0];
+  }
+
+  if (input === 'all') {
+    let message = '';
+    participantIDs.forEach((participantID, index) => {
+      message += `${index + 1}. ${participantID}\n`;
+    });
+    return api.sendMessage(message, threadID);
+  }
+
+  if (input === '-g' || input === 'group') {
+    return api.sendMessage(threadID, threadID);
+  }
+
+  return api.shareContact(id, id, threadID);
 };
